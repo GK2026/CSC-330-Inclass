8cda7a1c41e122b02493c9da05267f8d0b78ece9
kirbyg2@southernct.edu <kirbyg2@southernct.edu>
2025-09-19 14:26:11 +0000

    Fnished the Inclass Assigment

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
-//call back function
-serveStatic = function (req, res) {
-	let fileName = '.' + url.parse(req.url).pathname;
-	res.writeHead(200, {'Content-Type': 'text/plain' });
-	res.write("Place holder where the data should be sent");
-	res.end();
-}
- 
-const myserver = http.createServer(serveStatic); //create a server object
-myserver.listen(80, function() {console.log("Listening on port 80" )}); 
 
+// callback function
+const serveStatic = function (req, res) {
+    let fileName = '.' + url.parse(req.url).pathname; // line 6 already given
+
+    fs.readFile(fileName, function (err, data) {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.write('Error 404: resource not found');
+            res.end();
+        } else {
+            res.writeHead(200, { 'Content-Type': 'image/jpg' }); // simplest content type
+            res.write(data); // send file contents
+            res.end();
+        }
+    });
+};
+
+const myserver = http.createServer(serveStatic);
+
+myserver.listen(80, function () {
+    console.log("Listening on port 80");
+});
